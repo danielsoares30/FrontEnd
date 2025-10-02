e617c9a9efb5a759f817eeac33f0025e31a53afd
danielsoares30 <danielsoares3009007@gmail.com>
2025-10-02 07:56:06 -0300

    02/10

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Importa o nosso sistema de autenticação
 import { AuthProvider } from './contexts/AuthContext.jsx';
 
+
 // Importa os componentes de layout e proteção
 import Layout from './components/Layout.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
@@ -15,17 +16,17 @@ import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
 import Cadastro from './pages/Cadastro.jsx';
 import Dashboard from './pages/Dashboard.jsx';
-import PoliticaPrivacidade from './pages/PoliticaPrivacidade.jsx';
 import Relatorios from './pages/Relatorios.jsx';
-import SobreNos from './pages/SobreNos.jsx';
 import Transacoes from './pages/Transacoes.jsx';
-import TermosDeServico from './pages/TermosdeServico.jsx'; // Nome do arquivo corrigido
 import Perfil from './pages/Perfil.jsx';
 import Configuracoes from './pages/Configuracoes.jsx';
 import BuscarProjetos from './pages/BuscarProjetos.jsx';
 import EncontrarFreelancers from './pages/EncontrarFreelancer.jsx';
-import PaginaProposta from './pages/PaginaProposta.jsx'
+import PaginaProposta from './pages/PaginaProposta.jsx';
 
+// IMPORTAÇÃO DE NOVAS PÁGINAS
+import ProjectDetailsPage from './pages/ProjectDetailsPage.jsx';
+import InviteFreelancerPage from './pages/InviteFreelancerPage.jsx'; // Inclua o InviteFreelancerPage
 
 
 function App() {
@@ -37,11 +38,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/termos-de-servico" element={<TermosDeServico />} />
-          <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
-          <Route path="/sobre-nos" element={<SobreNos />} />
-          
-          {/* --- ROTAS PRIVADAS / PROTEGIDAS (acessíveis apenas após o login) --- */}
+
+          {/* --- ROTAS PRIVADAS / PROTEGIDAS --- */}
           <Route 
             path="/dashboard" 
             element={
@@ -55,10 +53,16 @@ function App() {
             <Route path="transacoes" element={<Transacoes />} />
             <Route path="perfil" element={<Perfil />} />
             <Route path="configuracoes" element={<Configuracoes />} />
+
             <Route path="buscar-projetos" element={<BuscarProjetos />} />
             <Route path="encontrar-freelancers" element={<EncontrarFreelancers />} /> 
-            <Route path="pagina-propostas" element={<PaginaProposta />} />
+            <Route path="pagina-proposta" element={<PaginaProposta />} />
+
+            {/* ROTA DINÂMICA: Detalhes do Projeto */}
+            <Route path="projeto/:id" element={<ProjectDetailsPage />} />
             
+            {/* ROTA DINÂMICA: Convidar Freelancer - GARANTINDO O NOME CORRETO DA ROTA */}
+            <Route path="convidar-freelancer/:freelancerId" element={<InviteFreelancerPage />} />
           </Route>
         </Routes>
       </AuthProvider>
