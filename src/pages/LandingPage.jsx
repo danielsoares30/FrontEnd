30c812b321777d30296b1047165babb98461ed8b
danielsoares30 <danielsoares3009007@gmail.com>
2025-10-22 12:45:56 -0300

    22/10

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -15,9 +15,9 @@ import iconCheck from '../assets/images/landingpage/icones/marca-de-verificacao.
 import iconMoney from '../assets/images/landingpage/icones/money-bag.png';
 
 // Ícones para a seção de depoimentos (AGORA COM LINKS)
-const depoimento1 = 'https://i.ibb.co/bF9Fh3D/ana.jpg';
-const depoimento2 = 'https://i.ibb.co/pW1Q6g6/carlos.jpg';
-const depoimento3 = 'https://i.ibb.co/d7z1y1J/julia.jpg';
+const depoimento1 = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+const depoimento2 = 'https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+const depoimento3 = 'https://plus.unsplash.com/premium_photo-1705018501151-4045c97658a3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 // Importe o seu ficheiro de estilo
 import '../assets/styles/landing.css'; 
